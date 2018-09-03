0ed5e9ccbc5d0657fd05cff947a5f75930fb0d62
cfairhurst <cfairhurst@mirabeau.nl>
2018-09-03 08:29:46 +0200

    Initial Commit

---
+++ src/main/java/nl/mirabeau/springbootpipeline/GenericErrorController.java
@@ -0,0 +1,19 @@
+package nl.mirabeau.springbootpipeline;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GenericErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
---
+++ src/main/java/nl/mirabeau/springbootpipeline/SpringBootPipelineApplication.java
@@ -0,0 +1,13 @@
+package nl.mirabeau.springbootpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPipelineApplication.class, args);
+	}
+
+}
